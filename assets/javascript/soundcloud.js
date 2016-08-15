9cf831c543426507030cae4b0427d9be115d142a
Kevin Whiteside 1 <kevindwhiteside@icloud.com>
2016-08-15 19:05:37 -0400

    added soundcloud.js and linked in player.html

---
+++ assets/javascript/soundcloud.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+
+
+
+	function getQueryVariable(variable)
+	{
+	       var query = window.location.search.substring(1);
+	       console.log(query);
+	       var vars = query.split("&");
+	       for (var i=0; i<vars.length; i++) {
+	               var pair = vars[i].split("=");
+	               if(pair[0] == variable){return pair[1];}
+	       }
+	       return(false);
+	}
+	var genreQuery = getQueryVariable("genre");
+	console.log(genreQuery);
+
+	$("#genre").text(genreQuery);
+})
