4501e33249e48f007b50cf7e2127659f1a6f6ece
Will McAuliff <womcauliff@gmail.com>
2016-08-16 21:04:03 -0400

    Added gif and still properties to Genre Object. Includes comments for gif TODOs

--- assets/javascript/genre.js
+++ assets/javascript/genre.js
@@ -1,20 +1,28 @@
 /**
  * Genre Object Constructor
+ *
+ * @param headerText string the text that will accompany the image/gif
+ * @param gif the path to the gif in the assets directory
+ * @param still the path to the still in the assets directory
+ * @param dataQuery the value that will be included in the url to the player page
  */
-function Genre(headerText, img, dataQuery) {
+function Genre(headerText, gif, still, dataQuery) {
 	var g = {
 		headerText: headerText,
-		img: img,
+		gif: gif,
+		still: still,
 		dataQuery: dataQuery,
 	}
 	return g;
 }
 
 var genres = [
-	new Genre("rock", "assets/images/house.png", "classicrock"),
-	new Genre("hip hop", "assets/images/house.png", "hiphop"),
-	new Genre("electronic", "assets/images/house.png", "electronic"),
-	new Genre("jazz", "assets/images/house.png", "jazz"),
+	new Genre("rock", "assets/images/house.png", "assets/images/house.png", "classicrock"),
+	new Genre("hip hop", "assets/images/house.png",  "assets/images/house.png", "hiphop"),
+	new Genre("electronic", "assets/images/house.png", "assets/images/house.png", "electronic"),
+	new Genre("jazz", "assets/images/house.png", "assets/images/house.png", "jazz"),
+	new Genre("future funk", "assets/images/house.png", "assets/images/house.png", "futurefunk"),
+	new Genre("vaporwave", "assets/images/house.png", "assets/images/house.png", "vaporwave")
 ];
 
 var columnsPerRow = 4;
@@ -28,10 +36,12 @@ $(document).ready(function() {
 	for (var i = 0; i < genres.length; i++) {
 
 		console.log(i + genres[i]);
-
+		//add data-state
+		//add data-still
+		//add data-animate
 
 		var genreHeading = $("<h4>").addClass("genre-heading").text(genres[i].headerText);
-		var genreImage = $("<div>").addClass("genre-image grayscale").css("background-image", "url('assets/images/house.png')");
+		var genreImage = $("<div>").addClass("genre-image grayscale").css("background-image", "url('" + genres[i].still + "')");
 
 		var genreWrapper = $("<div>").addClass("genre-wrapper");
 
@@ -55,4 +65,9 @@ $(document).ready(function() {
 			genreRow = $("<div>").addClass("row");
 		}
 	};
+});
+
+$(document).on("mouseover", ".genre-wrapper", function() {
+	//TODO
+	console.log($(this));
 });
